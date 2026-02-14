cf81d250a843f4a52b734c2d638a7fa16c04b9fe
cuahanganvatnhuy <cuahanganvatnhuy@gmail.com>
2026-02-14 17:40:36 +0700

    Fix missing imports in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate, Link, useLocation } from 'react-router-dom';
 import { VocabularyProvider } from './context/VocabularyContext';
 import './App.css';
 
